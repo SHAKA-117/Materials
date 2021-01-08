eb98be83f92d4d1c2eefce015ff420ffd3fe40ef
SHAKA-117 <66573118+SHAKA-117@users.noreply.github.com>
2021-01-08 08:34:17 -0600

    Update gcompris.sh

--- Desktops/GCompris/gcompris.sh
+++ Desktops/GCompris/gcompris.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-user$(whoami)
-cd /home/$user/gcompris-qt-1.0-Linux/bin/
-./gcompris-qt.sh
+user=$(whoami)
+echo $user
+xfce4-terminal -e /home/$user/gcompris-qt-1.0-Linux/bin/./gcompris-qt.sh
