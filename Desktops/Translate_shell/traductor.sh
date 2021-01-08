78841350cb1d9aaeccb2e0d00281beab532f8e77
SHAKA-117 <66573118+SHAKA-117@users.noreply.github.com>
2021-01-08 10:58:21 -0600

    Update traductor.sh

--- Desktops/Translate_shell/traductor.sh
+++ Desktops/Translate_shell/traductor.sh
@@ -1,22 +1,37 @@
 #!/usr/bin/bash
-echo "======Bienvenido======"
-echo ""
-echo "¿Qué palabra quieres traducir?"
-read palabra
-echo ""
-echo "¿En que idioma quieres traducirlo?"
-echo "=============================="
-echo "| Ingles:  en                |"
-echo "| Español: es                |"
-echo "| Aleman:  de                |"
-echo "| Japones: ja                |"
-echo "| Frances: fr                |"
-echo "| Portugues(Brasil): pt-BR   |"
-echo "| Ruso:    ru                |"
-echo "============================="
-read idioma
-echo ""
-sleep 1;
-echo ""
-echo "=============================="
-trans -shell :$idioma "$palabra"
+
+principal(){
+	echo "======Bienvenido======"
+	echo ""
+   	echo "¿Qué palabra quieres traducir?"
+    	read palabra
+   	echo ""
+    	echo "¿En que idioma quieres traducirlo?"
+    	echo "=============================="
+    	echo "| Ingles:  en                |"
+    	echo "| Español: es                |"
+    	echo "| Aleman:  de                |"
+    	echo "| Japones: ja                |"
+    	echo "| Frances: fr                |"
+    	echo "| Portugues(Brasil): pt-BR   |"
+    	echo "| Ruso:    ru                |"
+    	echo "============================="
+    	read idioma
+    	echo ""
+    	sleep 1;
+    	echo ""
+    	echo "=============================="
+    	trans -shell :$idioma "$palabra"
+	echo
+    	read -p "Necesita otra traducir otra palabra? s/n " res
+    	if [[ $res == S || $res == s ]]; then
+		clear
+        	principal
+        else
+        	clear
+                echo "Gracias por usar el tradcutor-shell"
+                exit
+         fi
+}
+
+principal
